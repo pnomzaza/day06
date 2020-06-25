724d92b61e1e04204747d587c29144fa8b74bc27
pnomzaza <67050335+pnomzaza@users.noreply.github.com>
2020-06-25 17:32:06 +0200

    day06

--- ex03/ft_rev_params.c
+++ ex03/ft_rev_params.c
@@ -6,11 +6,17 @@
 /*   By: pnomzaza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 16:34:37 by pnomzaza          #+#    #+#             */
-/*   Updated: 2020/06/25 16:39:42 by pnomzaza         ###   ########.fr       */
+/*   Updated: 2020/06/25 17:31:25 by pnomzaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putchar(char c);
+#include <unistd.h>
+
+
+void ft_putchar(char c)
+{
+	write(1,&c,1);
+}
 
 int main (int ac, char *av[])
 {
