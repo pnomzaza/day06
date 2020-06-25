372672edf497fb79b79d3ed4f47431f36818919d
pnomzaza <67050335+pnomzaza@users.noreply.github.com>
2020-06-25 16:25:47 +0200

    day06

--- ex00/libft_creator.sh
+++ ex00/libft_creator.sh
@@ -6,10 +6,10 @@
 #    By: pnomzaza <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/06/25 11:10:51 by pnomzaza          #+#    #+#              #
-#    Updated: 2020/06/25 13:59:18 by pnomzaza         ###   ########.fr        #
+#    Updated: 2020/06/25 16:20:31 by pnomzaza         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#!/bin/sh
-
-(gcc -Wall -Wextra -Werror -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c) && (ar rc libft.a *o) 
+gcc -Wall -Wextra -c ft*.c;
+ar rc libft.a ft*.o;
+ranlib libft.a;
